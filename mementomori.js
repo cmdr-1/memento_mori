198d1a6b8afdd5e96bf430a772f074f6204048a1
Mance Rah <mance.development@gmail.com>
2020-09-08 18:28:27 -0400

    Added icons and updated manifest.json

--- mementomori.js
+++ mementomori.js
@@ -1,3 +1,4 @@
+// Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
 function logTabs(tabs) {
     console.log(tabs)
   }
