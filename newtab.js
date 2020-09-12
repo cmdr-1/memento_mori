5756c567c29eab46cf0e14aec336164797cfd228
Mance Rah <mance.development@gmail.com>
2020-09-12 01:39:58 -0400

    Refined age calc method using JS functions

--- newtab.js
+++ newtab.js
@@ -25,43 +25,42 @@ function currentTime() {
   
 //-----------------------------------------------------------------------//
 
+
+
 // This is the the age calculator
-function currentAge() {
+function currentAge(birthDate) {
     
-    var DOB = "August 1, 1992";
-    // Date.parse() returns the number of milliseconds between the argument and Jan 1 1970
-    var msecBetweenDOBAnd1970 = Date.parse(DOB);
-    // Date.now() returns the number of milliseconds between the the current time and Jan 1 1970
-    var msecBetweenNowAnd1970 = Date.now();
-    var ageInMsec = msecBetweenNowAnd1970-msecBetweenDOBAnd1970;
+    birthDate = new Date("August 6, 1992");
+    var currentDate = new Date();
+    
+    var years = (currentDate.getFullYear() - birthDate.getFullYear());
+    var months = (currentDate.getMonth() - birthDate.getMonth());
+    var days = (currentDate.getDate() - birthDate.getDate());
+    var hours = currentDate.getHours();
+    var minutes = currentDate.getMinutes();
+    var seconds = currentDate.getSeconds();
+    
+    if (currentDate.getMonth() < birthDate.getMonth() || currentDate.getMonth() == birthDate.getMonth() && currentDate.getDate() < birthDate.getDate()) {
+        years--;
+    }
 
-    // Calculations done assuming that a month has 30 days and a year has 365 days - will update to be more accurate in future
-    var msec = ageInMsec;
-    var second = 1000;
-    var minute = second * 60;
-    var hour = minute * 60;
-    var day = hour * 24;
-    var month = day * 30; 
-    var year = day * 365;
-
-    // Long hand conversion of milliseconds to units of time, while rounding down allocating remainder for next unit of time
-    var years = Math.floor(msec/year);
-    var months = Math.floor((msec/year - years) * 12);
-    var days = Math.floor((((msec/year - years) * 12) - months) * 30);
-    var hours = Math.floor((((((msec/year - years) * 12) - months) * 30) - days) * 24);
-    var minutes = Math.floor((((((((msec/year - years) * 12) - months) * 30) - days) * 24) - hours) * 60);
-    var seconds = Math.round((((((((((msec/year - years) * 12) - months) * 30) - days) * 24) - hours) * 60) - minutes) * 60);
-        
+    if (months < 0) {
+        months = 11 + months;
+    }
+    if (days < 0) {
+        days = 30.44 + days;
+    }
+    
     var yearStr = " Years | "
     var monthStr = " "; 
     var dayStr = " ";
     var hourStr = " ";
     var minuteStr = " ";
     var secondStr = " "; 
-
+  
     if (months == 1) {
         monthStr = " Month";
-    } else if (month > 1 ) {
+    } else if (months > 1 ) {
         monthStr = " Months";
     } else if (months == 0) {
         months = "";
@@ -71,6 +70,8 @@ function currentAge() {
         dayStr = " Day | ";
     } else if (days > 1 ) {
         dayStr = " Days | ";
+    } else if (days == 0) {
+        days = ""; 
     }
     if (hours == 1) {
         hourStr = " Hour";
@@ -93,13 +94,12 @@ function currentAge() {
         seconds = "";
         minuteStr = " Minutes";
     }
+  
+    document.getElementById("memento").innerText = years + yearStr + months + monthStr + "\n" + Math.floor(days) + dayStr + hours + hourStr + "\n"
+                                                + minutes + minuteStr + seconds + secondStr;
 
-    document.getElementById("memento").innerText = years + yearStr + months + monthStr + "\n" + days + dayStr + hours + hourStr 
-                                                    + "\n" + minutes + minuteStr + seconds + secondStr;
-    
     // Refresh memento every 1000ms (1 second)
-    var age = setTimeout(function(){ currentAge() }, 1000); 
-  }
-
+    var age = setTimeout(function(){ currentAge(birthDate) }, 1000); 
+}
   // Initiating currentTime()
   currentAge();
