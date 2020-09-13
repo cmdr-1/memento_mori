847122139417a316e490134f4b3976f3b321095c
Mance Rah <mance.development@gmail.com>
2020-09-13 00:08:15 -0400

    added form to submit DOB

--- newtab.js
+++ newtab.js
@@ -24,67 +24,88 @@ function currentTime() {
   currentTime(); 
   
 //-----------------------------------------------------------------------//
+var birthDate = new Date(); 
 
+$("#button").click(function(){
+    var dob = $("#birthDate").val();
+    var birthdate = dob.split("/");
+
+    var parsedDate = (birthdate[0]+"/"+birthdate[1]+"/"+birthdate[2]);
+    console.log(parsedDate);
+    birthDate = parsedDate;
+});
 
 
 // This is the the age calculator
-function currentAge(birthDate) {
-    
-    birthDate = new Date("August 6, 1992");
+function currentAge() {
+    let birth_date = new Date(birthDate);
     var currentDate = new Date();
     
-    var years = (currentDate.getFullYear() - birthDate.getFullYear());
-    var months = (currentDate.getMonth() - birthDate.getMonth());
-    var days = (currentDate.getDate() - birthDate.getDate());
+    var years = (currentDate.getFullYear() - birth_date.getFullYear());
+    var months = (currentDate.getMonth() - birth_date.getMonth());
+    var days = currentDate.getDate() - birth_date.getDate();
     var hours = currentDate.getHours();
     var minutes = currentDate.getMinutes();
     var seconds = currentDate.getSeconds();
     
-    if (currentDate.getMonth() < birthDate.getMonth() || currentDate.getMonth() == birthDate.getMonth() && currentDate.getDate() < birthDate.getDate()) {
+    if (currentDate.getMonth() < birth_date.getMonth() || currentDate.getMonth() == birth_date.getMonth() && currentDate.getDate() < birth_date.getDate()) {
         years--;
     }
-
     if (months < 0) {
         months = 11 + months;
     }
     if (days < 0) {
-        days = 30.44 + days;
+        days = Math.floor(30.44 + days);
     }
     
-    var yearStr = " Years | "
+    var yearStr = " ";
     var monthStr = " "; 
     var dayStr = " ";
     var hourStr = " ";
     var minuteStr = " ";
     var secondStr = " "; 
-  
+    console.log(days);
+
+    if (years > 1 ) {
+        yearStr = " Years | ";
+    } else if (years == 0) {
+        years = "";
+        yearStr = ""; 
+    } else if (year == 0 && month == 0) {
+        years = "";
+        yearStr = "";
+    }
     if (months == 1) {
-        monthStr = " Month";
+        monthStr = " Month ";
     } else if (months > 1 ) {
-        monthStr = " Months";
+        monthStr = " Months ";
     } else if (months == 0) {
         months = "";
-        yearStr = " Years"; 
     }
     if (days == 1) {
-        dayStr = " Day | ";
+        dayStr = " Day";
     } else if (days > 1 ) {
-        dayStr = " Days | ";
+        dayStr = " Days";
     } else if (days == 0) {
-        days = ""; 
+        days = " ";
+        dayStr= " "; 
     }
     if (hours == 1) {
-        hourStr = " Hour";
+        hourStr = " Hour ";
     } else if (hours > 1 ) {
-        hourStr = " Hours";
+        hourStr = " Hours ";
     } else if (hours == 0) {
         hours = "";
-        dayStr = " Days" 
     }
     if (minutes == 1) {
         minuteStr = " Minute | ";
     } else if (minutes > 1 ) {
         minuteStr = " Minutes | ";
+    } else if (minutes == 0) {
+        minutes = "";
+    } else if ( minutes == 0 && hours == 0) {
+        minuteStr = "";
+        hourStr = "";
     }
     if (seconds == 1) {
         secondStr = " Second";
@@ -92,14 +113,13 @@ function currentAge(birthDate) {
         secondStr = " Seconds";
     } else if (seconds == 0 || seconds == 60) {
         seconds = "";
-        minuteStr = " Minutes";
+        minuteStr = " Minutes ";
     }
-  
-    document.getElementById("memento").innerText = years + yearStr + months + monthStr + "\n" + Math.floor(days) + dayStr + hours + hourStr + "\n"
-                                                + minutes + minuteStr + seconds + secondStr;
+    document.getElementById("memento").innerText = years + yearStr + months + monthStr + "| " + days + dayStr + "\n" + hours + hourStr
+                                                + "| " +  minutes + minuteStr + seconds + secondStr;
 
     // Refresh memento every 1000ms (1 second)
-    var age = setTimeout(function(){ currentAge(birthDate) }, 1000); 
+    var age = setTimeout(function(){ currentAge() }, 1000); 
 }
   // Initiating currentTime()
   currentAge();
