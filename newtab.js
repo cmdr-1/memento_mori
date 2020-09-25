6461879c587b74c7cef7de826af7f267b8b4a96d
Mance Rah <mance.development@gmail.com>
2020-09-24 22:35:55 -0400

    convert JQuery to JavaScript

--- newtab.js
+++ newtab.js
@@ -1,24 +1,30 @@
-// Clock display
-function currentTime() {
-    var date = new Date(); /* creating object of Date class */
-    var hour = date.getHours();
-    var min = date.getMinutes();
-    var sec = date.getSeconds();
-    hour = updateTime(hour);
-    min = updateTime(min);
-    sec = updateTime(sec);
-    document.getElementById("clock").innerText = " Current Time\n " + hour + " : " + min + " : " + sec; /* adding time to the div */
-      var t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
-  }
-  
-  function updateTime(k) {
-    if (k < 10) {
-      return "0" + k;
+function main() {
+    if (localStorage.length == 0) {
+        document.title = "Set up Memento Mori";
+        document.getElementById("main").innerHTML = 
+        `<div class="container">
+            <div class="title"><h3>Set up Memento Mori</h3></div>
+            <form>
+            <label for="birthDate">Enter your date of birth:</label>
+            <input type="date" name="Birthdate"id="birthDate" placeholder="YYYY-MM-DD" pattern="(?:19|20)[0-9]{2}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))"></input>
+            <button type="submit" id="button" onclick="saveDate()">Submit</button>
+            </form>
+        </div>`;
+      } else {
+        document.title = "Memento Mori";
+        document.getElementById("main").innerHTML = 
+        `<div class="container">
+            <div class="title"><h3>Memento Mori</h3></div>
+            <div id="memento-lg"></div>
+            <div id="memento-sm"></div>
+            <div id="clock""></div>
+            <button type="submit" id="clearDob" onclick="clearDate()">Reset DoB</button>
+        </div>`;
+        renderTimers();
     }
-    else {
-      return k;
-    }
-  }
+}
+
+main();
   
 //-----------------------------------------------------------------------//
 var birthDate; 
@@ -29,28 +35,25 @@ if (localStorage.getItem("birthDate")) {
     birthDate = new Date();
 }
 
-$("#button").click(function(){
-    var dob = $("#birthDate").val();
 
-    if (typeof dob !== null) {
-    const birthdate = dob.split("-");
-    var parsedDate = (birthdate[0]+"-"+birthdate[1]+"-"+birthdate[2]);
-    console.log(parsedDate);
-    birthDate = parsedDate;
-    localStorage.setItem("birthDate", birthDate);
-    } 
-});
+function saveDate() {
+    document.getElementById("button").click();    
+    var dob = document.getElementById("birthDate").value;
+    
+        if (typeof dob !== null) {
+        const birthdate = dob.split("-");
+        var parsedDate = (birthdate[0]+"-"+birthdate[1]+"-"+birthdate[2]);
+        console.log(parsedDate);
+        birthDate = parsedDate;
+        localStorage.setItem("birthDate", birthDate);
+        }
+} 
 
-$("#clearDob").click(function(){
+function clearDate() {
+    document.getElementById("clearDob").click();
     localStorage.clear();
     location.reload();
-});
-
-
-function renderTimers() {
-    currentAge();
-    currentTime();
-}
+};
 
 // This is the the age calculator
 function currentAge() {
@@ -117,4 +120,31 @@ function currentAge() {
 
     // Refresh memento every 1000ms (1 second)
     var age = setTimeout(function(){ currentAge() }, 1000); 
+}
+
+// Clock display
+function currentTime() {
+    var date = new Date(); /* creating object of Date class */
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+    document.getElementById("clock").innerText = " Current Time\n " + hour + " : " + min + " : " + sec; /* adding time to the div */
+      var t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
+  }
+  
+  function updateTime(k) {
+    if (k < 10) {
+      return "0" + k;
+    }
+    else {
+      return k;
+    }
+  }
+
+function renderTimers() {
+    currentAge();
+    currentTime();
 }
